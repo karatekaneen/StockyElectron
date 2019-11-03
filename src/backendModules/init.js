fb8bb6b299980d241568f43e89c8b137a4878c05
karatekaneen <bauhn_@hotmail.com>
2019-11-03 22:05:52 +0100

    changed name of event

--- src/backendModules/init.js
+++ src/backendModules/init.js
@@ -5,9 +5,9 @@ export const createInitApp = (ipcMain = _ipcMain, DataFetcher = _DataFetcher) =>
 	const initApp = () => {
 		const dataFetcher = new DataFetcher({ API_URL: 'http://localhost:4000/graphql?' })
 
-		ipcMain.on('asynchronous-message', async (event, { id }) => {
+		ipcMain.on('single-stock', async (event, { id }) => {
 			const resp = await dataFetcher.fetchStock({ id })
-			event.reply('asynchronous-reply', resp)
+			event.reply('single-stock-response', resp)
 		})
 	}
 	return initApp
