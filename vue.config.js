aac578e48f405ef124e839f4d78c07c11ac54722
karatekaneen <bauhn_@hotmail.com>
2019-11-17 23:26:15 +0100

    updated vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-	transpileDependencies: ['vuetify'],
-	configureWebpack: {
-		devtool: 'source-map'
-	}
+  "transpileDependencies": [
+    "vuetify"
+  ],
+  "configureWebpack": {
+    "devtool": "source-map"
+  }
 }
