95a7ecff44669b725450635fc3e1b3b90923563e
karatekaneen <bauhn_@hotmail.com>
2020-01-31 19:57:02 +0100

    Added getting of trade data

--- src/models/Trade.js
+++ src/models/Trade.js
@@ -1,7 +1,128 @@
+import _Signal from './Signal'
+
 class Trade {
-	constructor(x) {
-		// Do something
-		console.log(x)
+	constructor({ entry, exit, tradeData, quantity = 1 }, { Signal = _Signal } = {}) {
+		const isSignal = s => s instanceof Signal
+		const areDatesEqual = (d1, d2) => d1.getTime() !== d2.getTime()
+
+		if (!isSignal(entry) || !isSignal(exit)) {
+			throw new Error('Entry and exit must be Signal instances')
+		} else if (
+			areDatesEqual(entry.date, tradeData[0].date) ||
+			areDatesEqual(exit.date, tradeData[tradeData.length - 1].date)
+		) {
+			throw new Error('Invalid date range')
+		}
+
+		this.entry = entry
+		this.exit = exit
+		this.tradeData = tradeData
+		this.quantity = quantity
+		this.resultPerStock = this.roundNumber(exit.price - entry.price)
+		this.resultPercent = exit.price / entry.price - 1
+
+		this.setResultInCash()
+		this.setPositionValues()
+	}
+
+	/**
+	 * Returns the performance of the trade in percent while in market.
+	 * @returns {Array<Object>} The pricedata between entry and exit in percent
+	 */
+	get performancePercent() {
+		// TODO Make _performancePercent private when able to.
+		if (!this._performancePercent) {
+			this._performancePercent = this.calculatePerformancePercent({
+				entryPrice: this.entry.price,
+				tradeData: this.tradeData
+			})
+		}
+		return this._performancePercent
+	}
+
+	/**
+	 * Returns the performance of the trade in $ while in market.
+	 * @returns {Array<Object>} The pricedata between entry and exit cash
+	 */
+	get performanceCash() {
+		return (this._performanceCash = this.performancePercent.map(pricePoint => {
+			const output = { ...pricePoint }
+			output.open = this.roundNumber(this.entry.price * output.open * this.quantity)
+			output.high = this.roundNumber(this.entry.price * output.high * this.quantity)
+			output.low = this.roundNumber(this.entry.price * output.low * this.quantity)
+			output.close = this.roundNumber(this.entry.price * output.close * this.quantity)
+
+			return output
+		}))
+	}
+
+	/**
+	 * Sets the initial- and finalValue property to the quantity multiplied with the entry/exit price.
+	 * @returns {Trade} this
+	 */
+	setPositionValues() {
+		// ? Should this be a getter instead?
+		this.initialValue = this.roundNumber(this.quantity * this.entry.price)
+		this.finalValue = this.roundNumber(this.quantity * this.exit.price)
+
+		return this
+	}
+
+	/**
+	 * Sets the resultInCash property to the quantity multiplied with the result per stock.
+	 * @returns {Trade} this
+	 */
+	setResultInCash() {
+		// ? Should this be a getter instead?
+		this.resultInCash = this.roundNumber(this.quantity * this.resultPerStock)
+
+		return this
+	}
+
+	/**
+	 * Calculates the % performance for each bar in market
+	 * @param {Object} params
+	 * @param {Number} params.entryPrice
+	 * @param {Array<Object>} params.tradeData The array of price action between entry and exit
+	 * @returns {Array<Object>} The price action while in market in %
+	 */
+	calculatePerformancePercent({ entryPrice, tradeData }) {
+		return tradeData.map(pricePoint => {
+			const output = { ...pricePoint }
+
+			output.open = (pricePoint.open - entryPrice) / entryPrice
+			output.high = (pricePoint.high - entryPrice) / entryPrice
+			output.low = (pricePoint.low - entryPrice) / entryPrice
+			output.close = (pricePoint.close - entryPrice) / entryPrice
+
+			return output
+		})
+	}
+
+	/**
+	 * Sets the number of stocks and updates stored values
+	 * @param {Number} quantity The quantity of stocks traded
+	 * @returns {Trade} this
+	 */
+	setQuantity(quantity) {
+		this.quantity = quantity
+
+		// Update p/l & Update position values
+		this.setResultInCash().setPositionValues()
+
+		return this
+	}
+
+	/**
+	 * Converts from the annoying 13-decimal floats.
+	 * I'm too poor for this to make a significant error in the end anyways.
+	 * @param {Number} num The number to be converted to a proper number
+	 * @returns {Number} Rounded to 12 decimals
+	 * @todo Make private
+	 */
+	roundNumber(num) {
+		// TODO Make private when able to.
+		return Math.round(num * 1e10) / 1e10
 	}
 }
 
---
+++ src/models/__tests__/Trade.spec.js
@@ -0,0 +1,316 @@
+import Trade from '../Trade'
+import mockData from './__mocks__/tradeData.json'
+import Signal from '../Signal'
+// jest.mock('../Signal')
+
+// Convert the dates
+const entrySignal = mockData.entrySignal
+const exitSignal = mockData.exitSignal
+entrySignal.date = new Date(entrySignal.date)
+exitSignal.date = new Date(exitSignal.date)
+
+// Convert the price
+// entrySignal.price = Number(entrySignal.price)
+// exitSignal.price = Number(exitSignal.price)
+
+// Convert the dates to proper Date instances
+const tradeData = mockData.priceData.map(pricePoint => {
+	pricePoint.date = new Date(pricePoint.date)
+	return pricePoint
+})
+
+describe('Trade', () => {
+	let entry
+	let exit
+
+	beforeEach(() => {
+		// Create signal instances
+		entry = new Signal(entrySignal)
+		exit = new Signal(exitSignal)
+	})
+
+	it('Throws if entry and exit isnt signal instances', () => {
+		expect.assertions(1)
+
+		try {
+			const t = new Trade({ entry: null, exit: null, tradeData: [] })
+		} catch (err) {
+			expect(err.message).toBe('Entry and exit must be Signal instances')
+		}
+	})
+
+	it('Has a working constructor', () => {
+		const t = new Trade({
+			entry,
+			exit,
+			tradeData
+		})
+
+		expect(t instanceof Trade).toBe(true)
+	})
+
+	it('Throws if start date is not equal to entry signal date', () => {
+		expect.assertions(1)
+
+		entry.date = new Date('1999-01-02')
+
+		try {
+			const t = new Trade({
+				entry,
+				exit,
+				tradeData
+			})
+		} catch (err) {
+			expect(err.message).toBe('Invalid date range')
+		}
+	})
+
+	it('require end date to be equal to exit signal date', () => {
+		expect.assertions(1)
+
+		exit.date = new Date('1999-01-02')
+
+		try {
+			const t = new Trade({
+				entry,
+				exit,
+				tradeData
+			})
+		} catch (err) {
+			expect(err.message).toBe('Invalid date range')
+		}
+	})
+
+	it('Sets quantity to 1 by default', () => {
+		const t = new Trade({
+			entry,
+			exit,
+			tradeData
+		})
+
+		expect(t.quantity).toBe(1)
+	})
+
+	it('Can set quantity', () => {
+		const t = new Trade({
+			entry,
+			exit,
+			tradeData
+		})
+
+		expect(t.quantity).toBe(1)
+		t.setQuantity(1234)
+		expect(t.quantity).toBe(1234)
+	})
+
+	it('Calculates $ profit/loss per stock', () => {
+		const t = new Trade({
+			entry,
+			exit,
+			tradeData
+		})
+
+		expect(t.resultPerStock).toBe(9.1)
+	})
+
+	it('Calculates % profit/loss', () => {
+		const t = new Trade({
+			entry,
+			exit,
+			tradeData
+		})
+
+		expect(t.resultPercent).toBe(0.2643043857101366)
+		expect(t.roundNumber(t.resultPercent * t.entry.price)).toBe(t.resultInCash)
+	})
+
+	it('Calculates $ profit/loss', () => {
+		const t = new Trade({
+			entry,
+			exit,
+			tradeData
+		})
+
+		expect(t.resultPerStock).toBe(t.resultInCash)
+		t.setQuantity(20)
+	})
+
+	it('updates $ profit/loss when changing quantity', () => {
+		const t = new Trade({
+			entry,
+			exit,
+			tradeData
+		})
+
+		expect(t.resultInCash).toBe(t.resultPerStock)
+		t.setQuantity(20)
+		expect(t.resultInCash).toBe(t.resultPerStock * 20)
+	})
+
+	it('updates position values when changing quantity', () => {
+		const t = new Trade({
+			entry,
+			exit,
+			tradeData
+		})
+
+		expect(t.initialValue).toBe(t.entry.price)
+		expect(t.finalValue).toBe(t.exit.price)
+		t.setQuantity(20)
+		expect(t.initialValue).toBe(t.entry.price * 20)
+		expect(t.finalValue).toBe(t.exit.price * 20)
+	})
+
+	it('Can get initial position value', () => {
+		const t = new Trade({
+			entry,
+			exit,
+			tradeData,
+			quantity: 123
+		})
+
+		expect(t.initialValue).toBe(4234.89)
+	})
+	it('Can get final position value', () => {
+		const t = new Trade({
+			entry,
+			exit,
+			tradeData,
+			quantity: 123
+		})
+
+		expect(t.finalValue).toBe(5354.19)
+	})
+
+	it('Creates array with position value throughout the whole trade', () => {
+		const t = new Trade({
+			entry,
+			exit,
+			tradeData,
+			quantity: 123
+		})
+
+		const resp = t.calculatePerformancePercent({ entryPrice: entry.price, tradeData })
+		const performanceOHLC = resp.map(({ open, high, low, close }) => ({ open, high, low, close }))
+		const storedPerf = t.performancePercent.map(({ open, high, low, close }) => ({
+			open,
+			high,
+			low,
+			close
+		}))
+
+		expect(storedPerf).toEqual(performanceOHLC)
+		expect(performanceOHLC).toEqual([
+			{
+				close: 0.02788266047051992,
+				high: 0.03398199244844617,
+				low: -0.008132442637235003,
+				open: 0
+			},
+			{
+				close: -0.018007551553877357,
+				high: 0.018007551553877357,
+				low: -0.02004066221318611,
+				open: 0.018007551553877357
+			},
+			{
+				close: -0.03601510310775492,
+				high: -0.02410688353180361,
+				low: -0.04821376706360722,
+				open: -0.03194888178913742
+			},
+			{
+				close: 0.00377577693871631,
+				high: 0.018007551553877357,
+				low: -0.040081324426372425,
+				open: -0.03601510310775492
+			},
+			{
+				close: 0.02788266047051992,
+				high: 0.05198954400232353,
+				low: -0.004066221318617501,
+				open: -0.004066221318617501
+			},
+			{
+				close: 0.015974440894568606,
+				high: 0.03979088004647103,
+				low: 0.007841998257333812,
+				open: 0.03979088004647103
+			},
+			{
+				close: 0.015974440894568606,
+				high: 0.02991577112982867,
+				low: 0,
+				open: 0.00377577693871631
+			},
+			{
+				close: 0.02788266047051992,
+				high: 0.03194888178913742,
+				low: 0.011908219575951312,
+				open: 0.02004066221318611
+			},
+			{
+				close: 0.02991577112982867,
+				high: 0.03979088004647103,
+				low: 0.0058088875980250605,
+				open: 0.03194888178913742
+			},
+			{
+				close: 0.03979088004647103,
+				high: 0.04385710136508853,
+				low: 0.02004066221318611,
+				open: 0.02381643915190242
+			},
+			{
+				close: 0.03979088004647103,
+				high: 0.04792332268370603,
+				low: 0.03194888178913742,
+				open: 0.04792332268370603
+			},
+			{
+				close: 0.03194888178913742,
+				high: 0.03979088004647103,
+				low: 0.00377577693871631,
+				open: 0.011908219575951312
+			},
+			{
+				close: 0.02004066221318611,
+				high: 0.03601510310775492,
+				low: 0,
+				open: 0.26430438571013654
+			}
+		])
+	})
+
+	it('Calculates p/l for each bar', () => {
+		const t = new Trade({
+			entry,
+			exit,
+			tradeData,
+			quantity: 1000
+		})
+
+		const withoutDate = t.performanceCash.map(({ open, high, low, close }) => ({
+			open,
+			high,
+			low,
+			close
+		}))
+
+		expect(withoutDate).toEqual([
+			{ close: 960, high: 1170, low: -280, open: 0 },
+			{ close: -620, high: 620, low: -690, open: 620 },
+			{ close: -1240, high: -830, low: -1660, open: -1100 },
+			{ close: 130, high: 620, low: -1380, open: -1240 },
+			{ close: 960, high: 1790, low: -140, open: -140 },
+			{ close: 550, high: 1370, low: 270, open: 1370 },
+			{ close: 550, high: 1030, low: 0, open: 130 },
+			{ close: 960, high: 1100, low: 410, open: 690 },
+			{ close: 1030, high: 1370, low: 200, open: 1100 },
+			{ close: 1370, high: 1510, low: 690, open: 820 },
+			{ close: 1370, high: 1650, low: 1100, open: 1650 },
+			{ close: 1100, high: 1370, low: 130, open: 410 },
+			{ close: 690, high: 1240, low: 0, open: 9100 }
+		])
+	})
+})
