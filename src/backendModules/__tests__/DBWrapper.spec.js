b7d05d9e1972a919d0f407baf08dc453c90fa92c
karatekaneen <bauhn_@hotmail.com>
2020-02-28 19:09:43 +0100

    Added some tests for db wrapper

--- src/backendModules/__tests__/DBWrapper.spec.js
+++ src/backendModules/__tests__/DBWrapper.spec.js
@@ -2,18 +2,51 @@ import DBWrapper from '../DBWrapper'
 
 describe('DBWrapper', () => {
 	describe('getDocument', () => {
+		// Exposing outside of beforeEach
 		let DBWrapperMock
+		let get
+		let value
 		beforeEach(() => {
-			const value = jest.fn().mockResolvedValue({ data: 'cooool' })
-			const get = jest.fn().mockReturnValue({ value })
+			value = jest.fn().mockResolvedValue({ data: 'cooool' })
+			get = jest.fn().mockReturnValue({ value })
 			DBWrapperMock = jest.fn().mockResolvedValue({ get })
 		})
+
 		it('Works in vanilla case', async () => {
 			const resp = await DBWrapper.getDocument({
 				documentName: 'test',
 				DBWrapper: DBWrapperMock
 			})
 			expect(resp).toEqual({ data: 'cooool' })
+		})
+
+		it('Calls the db functions on the db', async () => {
+			const limit = jest.fn().mockReturnValue({ value })
+			get.mockReturnValue({ limit })
+
+			const resp = await DBWrapper.getDocument({
+				documentName: 'test',
+				DBWrapper: DBWrapperMock,
+				dbFunctions: [x => x.limit(5)]
+			})
+
+			expect(limit).toHaveBeenCalledWith(5)
+			expect(resp).toEqual({ data: 'cooool' })
+		})
+
+		it('Calls the db functions in order', async () => {
+			const sort = jest.fn().mockReturnValue({ value })
+			const limit = jest.fn().mockReturnValue({ sort, test: 42 })
+			get.mockReturnValue({ limit })
+
+			const resp = await DBWrapper.getDocument({
+				documentName: 'test',
+				DBWrapper: DBWrapperMock,
+				dbFunctions: [x => x.limit(5), x => x.sort(x.test)]
+			})
+
+			expect(limit).toHaveBeenCalledWith(5)
+			expect(sort).toHaveBeenCalledWith(42) // This would've been undefined if it wasn't in order.
 		})
 	})
 })
